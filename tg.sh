ccd345b18c084cc53322bc524b30a6d90783cf8e
Ian <ianpostuma@icloud.com>
2016-03-25 21:56:33 +0100

    minor update

--- tg.sh
+++ tg.sh
@@ -2,7 +2,7 @@
 now=$(date)
 to=$1
 body=$2
-LOGFILE="/home/ian/tg.log"
+LOGFILE="~/tg.log"
 telegram-cli -W server.pub -RD -e "msg $to $body"
 echo "$now Recipient=$to Message=$body" >> ${LOGFILE}
 echo "Finished" >> ${LOGFILE}

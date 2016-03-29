95958c201a90220b9d4da068dbe0bd415a9e4d3b
Ian <ianpostuma@icloud.com>
2016-03-29 16:28:46 +0200

    aggiunta delle mappe di google

---
+++ map.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#MYTEST="http://maps.google.com/maps/api/staticmap?size=512x512&maptype=roadmap&markers=color%3Ared%7C53.5375%2C-113.64&markers=color%3Ared%7C54.275%2C-120.32&markers=color%3Ablue%7C39.0%2C37.264&sensor=false"
+#?q=Hello world@53,-2&
+today=`date '+%Y-%m-%d'`
+yesterday=`date -d yesterday '+%Y-%m-%d'`
+MYBASE="http://www.ianpostuma.it/MyPlayGround/terremotimap.html?"
+cat terremoti$yesterday.txt | sed -e 's/ora/\nora/g' | grep "ora" | awk -v r=$yesterday '{print "q="r","$2","$4","$6"@"$8","$10"&"}'| tr -d "\n" > dump
+cat terremoti$today.txt | sed -e 's/ora/\nora\[/g' | grep "ora" | awk -v r=$yesterday '{print "q="r","$2","$4","$6"@"$8","$10"&"}' | tr -d "\n" >> dump
+MYWAYS=`cat dump`
+rm dump
+echo $MYBASE$MYWAYS
+firefox $MYBASE$MYWAYS
--- terremoti.sh
+++ terremoti.sh
@@ -3,8 +3,8 @@ SITO="http://cnt.rm.ingv.it/"
 today=`date '+%Y-%m-%d'`
 yesterday=`date -d yesterday '+%Y-%m-%d'`
 links2 -width 500 -dump -html-tables 0 $SITO > terremoti.txt
-cat terremoti.txt | grep -A 1 " "$today | grep -v $today | awk '{row="["NR"] ora "$(1);row=row" magnitudo "$(2);for(i=3; i<(NF-2);i++) {place=place" "$(i);};row=row" "place;place="";row=row" profondita " $(NF-2)"km";row=row" latitudine " $(NF-1);row=row" longitudine " $(NF) " ";print row;row="";}' | tr -d "\n" >terremoti$today.txt
-cat terremoti.txt | grep -A 1 " "$yesterday | grep -v $yesterday | awk '{row="["NR"] ora "$(1);row=row" magnitudo "$(2);for(i=3; i<(NF-2);i++) {place=place" "$(i);};row=row" "place; place="" ;row=row" profondita " $(NF-2)"km";row=row" latitudine " $(NF-1);row=row" longitudine " $(NF) " ";print row;row="";}' | tr -d "\n" > terremoti$yesterday.txt
+cat terremoti.txt | grep -A 1 " "$today | grep -v $today | awk '{row="["NR"] ora "$(1);row=row" magnitudo "$(2);for(i=3; i<(NF-2);i++) {place=place" "$(i);};row=row" profondita " $(NF-2)"km";row=row" latitudine " $(NF-1);row=row" longitudine " $(NF) " ";row=row" "place;place="";print row;row="";}' | tr -d "\n" >terremoti$today.txt
+cat terremoti.txt | grep -A 1 " "$yesterday | grep -v $yesterday | awk '{row="["NR"] ora "$(1);row=row" magnitudo "$(2);for(i=3; i<(NF-2);i++) {place=place" "$(i);};row=row" profondita " $(NF-2)"km";row=row" latitudine " $(NF-1);row=row" longitudine " $(NF) " ";row=row" "place; place="" ;print row;row="";}' | tr -d "\n" > terremoti$yesterday.txt
 #cat terremoti$today.txt
 #cat terremoti$yesterday.txt
 #cat terremoti$today.txt | tr -d "\n" > terremoti$today.txt
@@ -15,3 +15,10 @@ echo -e $data1
 echo -e $data2
 ./tg.sh "$1" "$data1"
 ./tg.sh "$1" "$data2"
+MYBASE="http://www.ianpostuma.it/MyPlayGround/terremotimap.html?"
+cat terremoti$yesterday.txt | sed -e 's/ora/\nora/g' | grep "ora" | awk -v r=$yesterday '{print "q="r","$2","$4","$6"@"$8","$10"&"}'| tr -d "\n" > dump
+cat terremoti$today.txt | sed -e 's/ora/\nora\[/g' | grep "ora" | awk -v r=$yesterday '{print "q="r","$2","$4","$6"@"$8","$10"&"}' | tr -d "\n" >> dump
+MYWAYS=`cat dump`
+rm dump
+data3="Mappa dei terremoti: "$MYBASE$MYWAYS" clicca sulle icone per altre informazioni."
+./tg.sh "$1" "$data3"
